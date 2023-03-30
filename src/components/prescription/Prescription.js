f82fe34f33ea7795cccaf294149d78b2e05bd3f2
Knoxx04 <onyiknoxx3@gmail.com>
2023-03-30 11:46:43 +0300

    Added fetch request, edit and delete buttons

--- src/components/prescription/Prescription.js
+++ src/components/prescription/Prescription.js
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Prescription = () => {
+  const [prescriptions, setPrescriptions] = useState([]);
+
+  useEffect(() => {
+    fetch('')
+      .then(response => response.json())
+      .then(data => setPrescriptions(data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
     <div>
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Dosage</th>
-          <th>Price</th>
-          <th>Patient name</th>
-        </tr>
-      </thead>
-    </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Dosage</th>
+            <th>Price</th>
+            <th>Patient name</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {prescriptions.map(prescription => (
+            <tr key={prescription.id}>
+              <td>{prescription.name}</td>
+              <td>{prescription.dosage}</td>
+              <td>{prescription.price}</td>
+              <td>{prescription.patientName}</td>
+              <td>
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
