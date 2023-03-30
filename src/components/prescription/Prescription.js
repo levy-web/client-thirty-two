01135baa051d9cd91d78ae014816e8f962ccadcf
Knoxx04 <onyiknoxx3@gmail.com>
2023-03-30 11:05:29 +0300

    created table with 4 columns

---
+++ src/components/prescription/Prescription.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Prescription = () => {
+  return (
+    <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Dosage</th>
+          <th>Price</th>
+          <th>Patient name</th>
+        </tr>
+      </thead>
+    </table>
+    </div>
+  )
+}
+
+export default Prescription
