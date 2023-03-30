7d813f09bbc3fe3a0bed2e3cb5c8603abda861b0
Skanner33 <mathewmageto@gmail.com>
2023-03-30 23:03:57 +0300

    created delete function

---
+++ src/components/DeletePatient.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+
+function DeletePatient({ patientId, onDelete }) {
+  const [patients, setPatients] = useState([]);
+
+  useEffect(() => {
+    fetch("/patients")
+      .then((response) => response.json())
+      .then((data) => setPatients(data));
+  }, []);
+
+  const handleDelete = (id) => {
+    fetch(`/patients/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        const newPatients = patients.filter((patient) => patient.id !== id);
+        setPatients(newPatients);
+        onDelete(patientId)
+      })
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <button onClick={handleDelete}>
+      Delete Patient
+    </button>
+  )
+}
+
+export default DeletePatient
--- src/components/ListPatients.js
+++ src/components/ListPatients.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import DeletePatient from "./DeletePatient";
 
 function ListPatients({ user }) {
 //   const [todos, setTodos] = useState([]);
@@ -63,77 +64,9 @@ function ListPatients({ user }) {
                   the bulk of the card's content.
                 </p>
                 <button className="btn btn-primary btn-sm">Edit Patient</button>
+                <DeletePatient>
                 <button className="btn btn-danger btn-sm">Delete Patient</button>
-              </div>
-            </div>
-
-            <div className="pt-0">
-              <div className="card ms-1 mb-1" style={{ width: "18rem" }}>
-                <div className="card-body">
-                  <h5 clclassNameass="card-title">Patient title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <button className="btn btn-primary btn-sm">Edit Patient</button>
-                  <button className="btn btn-danger btn-sm">Delete Patient</button>
-                </div>
-              </div>
-            </div>
-
-            <div className="pt-2">
-              <div className="card ms-1  mb-1" style={{ width: "18rem" }}>
-                <div className="card-body">
-                  <h5 clclassNameass="card-title">Card Patient</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <button className="btn btn-primary btn-sm">Edit Patient</button>
-                  <button className="btn btn-danger btn-sm">Delete Patient</button>
-                </div>
-              </div>
-            </div>
-
-            <div className="pt-2">
-              <div className="card ms-1  mb-1" style={{ width: "18rem" }}>
-                <div className="card-body">
-                  <h5 clclassNameass="card-title">Patient title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <button className="btn btn-primary btn-sm">Edit Patient</button>
-                  <button className="btn btn-danger btn-sm">Delete Patient</button>
-                </div>
-              </div>
-            </div>
-
-            <div className="pt-2">
-              <div className="card ms-1  mb-1" style={{ width: "18rem" }}>
-                <div className="card-body">
-                  <h5 clclassNameass="card-title">Patient title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <button className="btn btn-primary btn-sm">Edit Patient</button>
-                  <button className="btn btn-danger btn-sm">Delete Patient</button>
-                </div>
-              </div>
-            </div>
-
-            <div className="pt-2">
-              <div className="card ms-1  mb-1" style={{ width: "18rem" }}>
-                <div className="card-body">
-                  <h5 clclassNameass="card-title">Patient title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <button className="btn btn-primary btn-sm">Edit Patient</button>
-                  <button className="btn btn-danger btn-sm">Delete Patient</button>
-                </div>
+                </DeletePatient>
               </div>
             </div>
           </div>
