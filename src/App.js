894216ded4134b00a97d3d8b64a7c4372ef91a10
Skanner33 <mathewmageto@gmail.com>
2023-03-30 12:03:29 +0300

    modified app.js and index css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Login from "./components/Auth";
+import Register from "./components/Auth";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+    <Navbar/> 
+
+   <Routes>
+    <Route exact path="/" element={<Login/>} />
+    <Route exact path="/login" element={<Register/>} />
+   </Routes>
+   
+   </BrowserRouter>
   );
 }
 
